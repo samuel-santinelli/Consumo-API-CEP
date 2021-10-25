be3f5822db48622f34b03724cdd2f8acddbb0440
Samuel Santinelli <samuelsantinelli@gmail.com>
2021-10-25 15:47:07 -0300

    Small project carried out to work with apis, in the case of zip code.

--- cep.js
+++ cep.js
@@ -7,17 +7,28 @@ const pesquisarCep = async (cep) => {
     return data;
 };
 
+const cepValido = (cep) => /^[0-9]{8}$/.test(cep);
+
+const limparFormulario = () => {
+    document.querySelector('#endereco').value = '';
+    document.querySelector('#bairro').value = '';
+    document.querySelector('#cidade').value = '';
+    document.querySelector('#estado').value = '';
+
+};
+
 const preencherFormulario = async (evento) => {
     const cep = evento.target.value;
-
-    const infoCep = await pesquisarCep(cep);
-
-    document.querySelector('#endereco').value = infoCep.logradouro;
-    document.querySelector('#bairro').value = infoCep.bairro;
-    document.querySelector('#cidade').value = infoCep.localidade;
-    document.querySelector('#estado').value = infoCep.uf;
-
-    console.log(infoCep);
+    limparFormulario();
+    if (cepValido(cep)){
+        const infoCep = await pesquisarCep(cep);
+        document.querySelector('#endereco').value = infoCep.logradouro;
+        document.querySelector('#bairro').value = infoCep.bairro;
+        document.querySelector('#cidade').value = infoCep.localidade;
+        document.querySelector('#estado').value = infoCep.uf;
+    }else{
+        document.querySelector('#endereco').value = "cep invalido, por favor digite um cep valido!";
+    }
 };
 
 document.querySelector('#cep').addEventListener('focusout', preencherFormulario);
